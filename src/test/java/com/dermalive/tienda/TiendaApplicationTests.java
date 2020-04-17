dc931822a7890b78777ac57d47fa293eda43f41e
unknown <vmelanier@gmail.com>
2020-04-16 21:16:26 -0500

    commit inicial

---
+++ src/test/java/com/dermalive/tienda/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dermalive.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
