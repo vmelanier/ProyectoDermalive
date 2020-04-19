77217d43d4c2022a594f5d6c386df05357e6ddb1
unknown <vmelanier@gmail.com>
2020-04-19 18:19:29 -0500

    Controladores y vista inicial

---
+++ src/main/java/com/dermalive/tienda/controllers/HomeControllers.java
@@ -0,0 +1,14 @@
+package com.dermalive.tienda.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeControllers {
+
+    @RequestMapping("/")
+    public String index (){
+        return "index";
+    }
+
+}
