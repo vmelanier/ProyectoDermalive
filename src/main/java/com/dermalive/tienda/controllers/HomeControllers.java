c0d5138122b475d96b26b73d21b6163384be344f
unknown <vmelanier@gmail.com>
2020-04-20 23:27:50 -0500

    Se configuro HomeControllers.java

--- src/main/java/com/dermalive/tienda/controllers/HomeControllers.java
+++ src/main/java/com/dermalive/tienda/controllers/HomeControllers.java
@@ -1,5 +1,9 @@
 package com.dermalive.tienda.controllers;
 
+import com.dermalive.tienda.db.HibernateSession;
+import com.dermalive.tienda.models.User;
+
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +12,19 @@ public class HomeControllers {
 
     @RequestMapping("/")
     public String index (){
+
+        User user = new User ();
+        user.setFirstName("Migu");
+        user.setLastName("Ang");
+        user.setUsername("miguel.ang");
+        user.setPassword("123456789");
+
+        Session session = HibernateSession.open();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
+
         return "index";
     }
 
