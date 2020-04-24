c1480e4ab8c20dcacf2e8895c5d68fb887fd6a98
unknown <vmelanier@gmail.com>
2020-04-24 00:12:47 -0500

    Se modifico el archivo User.java

--- src/main/java/com/dermalive/tienda/models/User.java
+++ src/main/java/com/dermalive/tienda/models/User.java
@@ -1,55 +1,23 @@
 package com.dermalive.tienda.models;
-
-import java.sql.Date;
-
 import javax.persistence.*;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "users")
-
 public class User {
 
     @Id
-    @Column(name = "id")
+    @Column(name="id",updatable = false, nullable=false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    
-    private String first_name;
-    private String last_name;
+    private Long id;
+
+    @Column(name="username", nullable=false)
     private String username;
+
+    @Column(name="password", nullable=false)
     private String password;
-    private String image_url;
-    private String remember_token;
 
-    @CreationTimestamp
-    private Date created_at;
-
-    @UpdateTimestamp
-    private Date updated_at;
-    private Date deleted_at;
-
-
-    public Integer getId(){
+    public Long getId (){
         return id;
-    }
-
-    public String getFirstName() {
-        return first_name;
-    }
-
-    public void setFirstName(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLastName() {
-        return last_name;
-    }
-
-    public void setLastName(String last_name) {
-        this.last_name = last_name;
     }
 
     public String getUsername() {
@@ -66,46 +34,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getImageUrl() {
-        return image_url;
-    }
-
-    public void setImageUrl(String image_url) {
-        this.image_url = image_url;
-    }
-
-    public String getRememberToken() {
-        return remember_token;
-    }
-
-    public void setRememberToken(String remember_token) {
-        this.remember_token = remember_token;
-    }
-
-    public Date getCreatedAt() {
-        return created_at;
-    }
-
-    public void setCreatedAt(Date created_at) {
-        this.created_at = created_at;
-    }
-
-    public Date getUpdatedAt() {
-        return updated_at;
-    }
-
-    public void setUpdatedAt(Date updated_at) {
-        this.updated_at = updated_at;
-    }
-
-    public Date getDeletedAt() {
-        return deleted_at;
-    }
-
-    public void setDeletedAt(Date deleted_at) {
-        this.deleted_at = deleted_at;
     }
 
 }
